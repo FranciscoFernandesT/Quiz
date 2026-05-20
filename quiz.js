edb7d9b3c1d1092c9de3d361f64f48394c94b5bc
FranciscoFernandesT <francisco32p1real@gmail.com>
2026-05-19 21:00:48 -0300

    Add files via upload

--- quiz.js
+++ quiz.js
@@ -1,23 +1,44 @@
 const perguntas = [
-{ pergunta: 'Quem pintou a Mona Lisa?', opcoes:
-['Michelangelo', 'Airton Senna', 'Jailson Mendes', 'Leonardo Da Vinci'], correta: 4},
-{ pergunta: 'Em que linguagem isso foi feito?', opcoes:
-['Portugues', 'JavaScript', 'HTML', 'Da Vinci'], correta: 2 },
-{ pergunta: 'O que é a vida?', opcoes:
-['Energia', 'Felicidade', 'Sofrimento', 'Todas'], correta: 4 },
-{ pergunta: 'Quantas vezes você errou?', opcoes:
-['1', '2', 'Sim', '3'], correta: 3 },
-{ pergunta: 'Matemático cego tem problema?', opcoes:
-['Divisão', 'Multiplicacao', 'Adicao', 'Subtração'], correta: 1 }
+    { pergunta: 'Quem pintou a Mona Lisa?', opcoes:
+        ['Michelangelo', 'Airton Senna', 'Jailson Mendes', 'Leonardo Da Vinci'], correta: 3 },
+    { pergunta: 'Em que linguagem isso foi feito?', opcoes:
+        ['Portugues', 'JavaScript', 'HTML', 'Da Vinci'], correta: 1 },
+    { pergunta: 'O que é a vida?', opcoes:
+        ['Energia', 'Felicidade', 'Sofrimento', 'Todas'], correta: 3 },
+    { pergunta: 'Qual o ano de criação do JS?', opcoes:
+        ['1995', '2000', '1990', '500 a.C.'], correta: 0 },
+    { pergunta: 'Qual problema tem o matemático cego?', opcoes:
+        ['Divisão', 'Multiplicacao', 'Adicao', 'Subtração'], correta: 0 },
+    { pergunta: 'Ratos são:', opcoes:
+        ['Belisários', 'Quentes', 'Lindos', 'Asquerosos'], correta: 1 }
 ]
 
-const idx = Math.floor(
-    Math.random() * perguntas.length
-);
-const q = perguntas[idx];
-tituloEl.textContent = q.pergunta;
 
-BigInt.addEventListener ('click', ()
-=> { if (indice === q.correta) {
-    feedbackEl.textContent = 'Correto!'; } else {
-        feedbackEl.textContent = 'Errado!';}} );
+
+
+
+
+
+
+    const indiceAleatorio = Math.floor( Math.random() * perguntas.length )
+    const questaoAtual = perguntas[indiceAleatorio];
+
+    document.getElementById('pergunta').textContent = questaoAtual.pergunta;
+    const botoes = document.querySelectorAll('.btn-opcao');
+    const feedback = document.getElementById('feedback');
+
+    botoes.forEach((btn, index) => {
+        btn.textContent = questaoAtual.opcoes[index];
+
+        btn.addEventListener('click', () => {
+            if (index === questaoAtual.correta) {
+                btn.classList.add('correto');
+                feedback.textContent = "Certa Resposta!";
+                feedback.style.color = "#4CAF50";
+            } else {
+                btn.classList.add('errado');
+                feedback.textContent = "Errou!";
+                feedback.style.color = "#F44336";
+            }
+        })
+    } );
